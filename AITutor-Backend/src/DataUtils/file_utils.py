d22afcdc788017e8e57b37134167e9b263058012
Jonathan Koch <Jonathan.Koch@caemilusa.com>
2023-10-30 01:07:47 -0400

    adding template for planner prompts

---
+++ AITutor-Backend/src/DataUtils/file_utils.py
@@ -0,0 +1,10 @@
+from pptx import Presentation
+import glob
+
+def read_pptx_file(pptx_file):
+    content = ''
+    prs = Presentation(pptx_file)
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if hasattr(shape, "text"):
+                content+=shape.text+'\n'
